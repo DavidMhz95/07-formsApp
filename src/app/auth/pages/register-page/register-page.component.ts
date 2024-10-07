40287f55a251cf61dba263520af8d38f0ab764b5
Davidmhz95 <davidmhz95@gmail.com>
2024-10-08 00:16:22 +0200

    Validators and functions

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent{
 
+  private fb = new FormBuilder();
+
+  public myform = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    username: ['',[Validators.required, cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)] ],
+    password2: ['', Validators.required],
+    })
+
+
+    isValidField(field: string) {
+      //TODO obtenner de un servicio
+    }
+
+    onSubmit(){
+      if(this.myform.invalid){
+        this.myform.markAllAsTouched();
+        return;
+      }
+      console.log(this.myform.value);
+    }
 }
--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+
+
+  ngOnInit(): void {
+    this.myform.reset({
+      ...this.person
+    })
+  }
+
+
+  private fb = new FormBuilder();
+  public myform: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [true, Validators.requiredTrue],
+  });
+
+  public person = {
+    gender:'F',
+    wantNotifications: false
+  }
+
+  onSave() {
+    if (this.myform.invalid) {
+      this.myform.markAllAsTouched();
+      return;
+    }
+
+    const {termsAndConditions, ...newPerson} = this.myform.value;
+    this.person = newPerson;
+    console.log(this.myform.value);
+
+    console.log(this.person);
+
+
+  }
+
+  isValidField(field: string): boolean | null {
+    return this.myform.controls[field].errors && this.myform.controls[field].touched
+  }
+
+  getErrorMessage(field: string): string | null {
+    if (!this.myform.controls[field].errors) return null
+
+    const errors = this.myform.controls[field].errors || {}
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'This field is required'
+        case 'minlength':
+          return `This field must have at least ${errors['minlength'].requiredLength} characters`
+        case 'min':
+          return `The minimum valid value is ${errors['min'].min}`
+
+      }
+
+    }
+
+    return null
+  }
 
 }
